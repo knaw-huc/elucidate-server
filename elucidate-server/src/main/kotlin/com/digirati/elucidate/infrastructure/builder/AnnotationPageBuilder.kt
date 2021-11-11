de28a2526384c9a2269c36bea2e4d62962436ce4
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2021-11-11 13:45:31 +0100

    refactoring: use listOf() && mapOf()

--- elucidate-server/src/main/kotlin/com/digirati/elucidate/infrastructure/builder/AnnotationPageBuilder.kt
+++ elucidate-server/src/main/kotlin/com/digirati/elucidate/infrastructure/builder/AnnotationPageBuilder.kt
@@ -1,8 +1,16 @@
 package com.digirati.elucidate.infrastructure.builder
 
-import com.digirati.elucidate.common.infrastructure.constants.ActivityStreamConstants
-import com.digirati.elucidate.common.infrastructure.constants.JSONLDConstants
-import com.digirati.elucidate.common.infrastructure.constants.XMLSchemaConstants
+import com.digirati.elucidate.common.infrastructure.constants.ActivityStreamConstants.URI_ITEMS
+import com.digirati.elucidate.common.infrastructure.constants.ActivityStreamConstants.URI_NEXT
+import com.digirati.elucidate.common.infrastructure.constants.ActivityStreamConstants.URI_ORDERED_COLLECTION_PAGE
+import com.digirati.elucidate.common.infrastructure.constants.ActivityStreamConstants.URI_PART_OF
+import com.digirati.elucidate.common.infrastructure.constants.ActivityStreamConstants.URI_PREV
+import com.digirati.elucidate.common.infrastructure.constants.ActivityStreamConstants.URI_START_INDEX
+import com.digirati.elucidate.common.infrastructure.constants.JSONLDConstants.ATTRIBUTE_ID
+import com.digirati.elucidate.common.infrastructure.constants.JSONLDConstants.ATTRIBUTE_LIST
+import com.digirati.elucidate.common.infrastructure.constants.JSONLDConstants.ATTRIBUTE_TYPE
+import com.digirati.elucidate.common.infrastructure.constants.JSONLDConstants.ATTRIBUTE_VALUE
+import com.digirati.elucidate.common.infrastructure.constants.XMLSchemaConstants.URI_NON_NEGATIVE_INTEGER
 import com.digirati.elucidate.common.infrastructure.util.PaginationUtils.calculateLastPage
 import com.digirati.elucidate.common.model.annotation.AbstractAnnotation
 import com.digirati.elucidate.common.model.annotation.AbstractAnnotationPage
@@ -29,95 +37,39 @@ class AnnotationPageBuilder<A : AbstractAnnotation, P : AbstractAnnotationPage>(
         val from = min(annotationSelection.size, max(0, page * pageSize))
         val to = min(annotationSelection.size, (page + 1) * pageSize)
         annotationSelection = annotationSelection.subList(from, to)
-        val jsonMap: MutableMap<String, Any> = HashMap()
-        jsonMap[JSONLDConstants.ATTRIBUTE_TYPE] = object : ArrayList<String>() {
-            init {
-                add(ActivityStreamConstants.URI_ORDERED_COLLECTION_PAGE)
-            }
-        }
+
+        val jsonMap: MutableMap<String, Any> = mutableMapOf()
+        jsonMap[ATTRIBUTE_TYPE] = listOf(URI_ORDERED_COLLECTION_PAGE)
+
         val partOfIri = annotationCollectionIriBuilder.buildAnnotationCollectionIri()
-        jsonMap[ActivityStreamConstants.URI_PART_OF] =
-            object : ArrayList<Map<String, Any>>() {
-                init {
-                    add(object : HashMap<String, Any>() {
-                        init {
-                            put(JSONLDConstants.ATTRIBUTE_ID, partOfIri)
-                        }
-                    })
-                }
-            }
-        jsonMap[ActivityStreamConstants.URI_START_INDEX] =
-            object : ArrayList<Map<String, Any>>() {
-                init {
-                    add(object : HashMap<String, Any>() {
-                        init {
-                            put(JSONLDConstants.ATTRIBUTE_TYPE, XMLSchemaConstants.URI_NON_NEGATIVE_INTEGER)
-                            put(JSONLDConstants.ATTRIBUTE_VALUE, from)
-                        }
-                    })
-                }
-            }
+        jsonMap[URI_PART_OF] = listOf(mapOf(ATTRIBUTE_ID to partOfIri))
+
+        jsonMap[URI_START_INDEX] = listOf(
+            mapOf(
+                ATTRIBUTE_TYPE to URI_NON_NEGATIVE_INTEGER,
+                ATTRIBUTE_VALUE to from
+            )
+        )
         if (page > 0) {
             val prevIri = annotationPageIriBuilder.buildAnnotationPageIri(page - 1, embeddedDescriptions)
-            jsonMap[ActivityStreamConstants.URI_PREV] = object : ArrayList<Map<String, Any>>() {
-                init {
-                    add(object : HashMap<String, Any>() {
-                        init {
-                            put(JSONLDConstants.ATTRIBUTE_ID, prevIri)
-                        }
-                    })
-                }
-            }
+            jsonMap[URI_PREV] = listOf(mapOf(ATTRIBUTE_ID to prevIri))
         }
         if (page < lastPage) {
             val nextIri = annotationPageIriBuilder.buildAnnotationPageIri(page + 1, embeddedDescriptions)
-            jsonMap[ActivityStreamConstants.URI_NEXT] =
-                object : ArrayList<Map<String, Any>>() {
-                    init {
-                        add(object : HashMap<String, Any>() {
-                            init {
-                                put(JSONLDConstants.ATTRIBUTE_ID, nextIri)
-                            }
-                        })
-                    }
-                }
+            jsonMap[URI_NEXT] = listOf(mapOf(ATTRIBUTE_ID to nextIri))
         }
         if (embeddedDescriptions) {
             val annotationDescriptions = convertToDescriptions(annotationSelection)
-            jsonMap[ActivityStreamConstants.URI_ITEMS] = object : ArrayList<Map<String, Any>>() {
-                init {
-                    add(object : HashMap<String, Any>() {
-                        init {
-                            put(
-                                JSONLDConstants.ATTRIBUTE_LIST,
-                                object : ArrayList<Map<String, Any>>() {
-                                    init {
-                                        addAll(annotationDescriptions)
-                                    }
-                                })
-                        }
-                    })
-                }
-            }
+            jsonMap[URI_ITEMS] = listOf(
+                mapOf(
+                    ATTRIBUTE_LIST to annotationDescriptions
+                )
+            )
         } else {
             val annotationIris = convertToIris(annotationSelection)
-            jsonMap[ActivityStreamConstants.URI_ITEMS] = object : ArrayList<Map<String, Any>>() {
-                init {
-                    add(object : HashMap<String, Any>() {
-                        init {
-                            put(
-                                JSONLDConstants.ATTRIBUTE_LIST,
-                                object : ArrayList<Map<String, Any>>() {
-                                    init {
-                                        addAll(annotationIris)
-                                    }
-                                })
-                        }
-                    })
-                }
-            }
+            jsonMap[URI_ITEMS] = listOf(mapOf(ATTRIBUTE_LIST to annotationIris))
         }
-        val annotationPage = annotationPageConverter.convertToAnnotationPage(jsonMap.toMutableMap())
+        val annotationPage = annotationPageConverter.convertToAnnotationPage(jsonMap)
         return ServiceResponse(ServiceResponse.Status.OK, annotationPage)
     }
 
@@ -130,7 +82,7 @@ class AnnotationPageBuilder<A : AbstractAnnotation, P : AbstractAnnotationPage>(
     }
 
     private fun convertToIris(annotations: List<A>): List<Map<String, Any>> {
-        return annotations.map { a -> mapOf(JSONLDConstants.ATTRIBUTE_ID to a.jsonMap!![JSONLDConstants.ATTRIBUTE_ID]!!) }
+        return annotations.map { a -> mapOf(ATTRIBUTE_ID to a.jsonMap!![ATTRIBUTE_ID]!!) }
 //        val iris: MutableList<Map<String, Any>> = ArrayList()
 //        for (annotation in annotations) {
 //            iris.add(mapOf(JSONLDConstants.ATTRIBUTE_ID to annotation.jsonMap!![JSONLDConstants.ATTRIBUTE_ID]!!))
